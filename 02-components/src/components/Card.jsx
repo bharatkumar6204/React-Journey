66afe8031b0d37ed9c03df893d0950b05097d790
bharatkumar6204 <bbharatkusavansi@gmail.com>
2026-05-28 16:32:53 +0530

    02-components

---
+++ 02-components/src/components/Card.jsx
@@ -0,0 +1,15 @@
+ import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+        <div className='card'>
+            <img src="https://images.unsplash.com/photo-1693724316310-2be00610cac6?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDg1fHRvd0paRnNrcEdnfHxlbnwwfHx8fHw%3D" alt="" />
+        <h2>Bharat Kumar</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, quae.</p>
+      </div>
+    </div>
+  )
+}
+
+export default Card 
