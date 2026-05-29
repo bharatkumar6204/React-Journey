fe5816d1e0a10dc4ab8366765518cf804784e6c8
bharatkumar6204 <bbharatkusavansi@gmail.com>
2026-05-29 19:33:49 +0530

    06-tailwind-css

---
+++ 06-tailwind-css/src/App.jsx
@@ -0,0 +1,10 @@
+
+const App = () => {
+  return (
+    <div className="bg-red-600 px-10 py-5">
+      <h1 className="text-2xl text-white">Hello This is Tailwind css </h1>
+    </div>
+  )
+}
+
+export default App
