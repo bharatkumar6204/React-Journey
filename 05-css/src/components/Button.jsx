a3bd447927b918d4ee62b8a925bec6cf29dc94fc
bharatkumar6204 <bbharatkusavansi@gmail.com>
2026-05-29 19:00:41 +0530

    05-css

---
+++ 05-css/src/components/Button.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/Button.css'
+
+const Button = () => {
+  return (
+    <div className='btn'>
+       <h2>This is Button</h2>
+       <button className='bt'>Login</button>
+    </div>
+  )
+}
+
+export default Button
