9226417ae1d4f25f76c269d050d317a1f31a20e7
bharatkumar6204 <bbharatkusavansi@gmail.com>
2026-05-30 19:10:45 +0530

    07-function

--- 07-function/src/App.jsx
+++ 07-function/src/App.jsx
@@ -1,18 +1,38 @@
+// const App = () => {
+//   function btnClicked(){
+// console.log('button is clicked');
+//   }
+//   function mouseEnter(){
+//     console.log('change text');
+
+//   }
+//   return (
+//     <div>
+//       <h1>Hello, Bharat</h1>
+//       <button onMouseEnter={mouseEnter} onClick={btnClicked}>change User</button>
+//     </div>
+//   )
+// }
+
+// export default App
+
+// Button click par value print karna ho
+let name = "";
 
 const App = () => {
-  function btnClicked(){
-console.log('button is clicked');
+  function inputChnage(e) {
+    name = e.target.value;
   }
-  function mouseEnter(){
-    console.log('change text');
-    
+  function btnClicked() {
+    console.log(name);
   }
   return (
-    <div>
-      <h1>Hello, Bharat</h1>
-      <button onMouseEnter={mouseEnter} onClick={btnClicked}>change User</button>
-    </div>
-  )
-}
+    <div className="text">
+      <input onChange={inputChnage} type="text" placeholder="Enter Your Name" />
 
-export default App
+      <button onClick={btnClicked}>Click</button>
+    </div>
+  );
+};
+
+export default App;
