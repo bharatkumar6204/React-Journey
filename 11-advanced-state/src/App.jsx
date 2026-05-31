b8523b2c30a18a00118c9c3770f696e89a818c47
bharatkumar6204 <bbharatkusavansi@gmail.com>
2026-05-31 23:46:47 +0530

    11-advanced-state

--- 11-advanced-state/src/App.jsx
+++ 11-advanced-state/src/App.jsx
@@ -1,19 +1,41 @@
 import { useState } from "react"
 
 
-const App = () => {
-  const [num, setNum] = useState([10,20,30,40])
+// const App = () => {
+//   const [num, setNum] = useState([10,20,30,40])
+// // arr ke ander change
+//   const btnClicked=()=>{
+//   const  newNum = [...num]
+//   newNum.push(99) 
+//   console.log(newNum);
+//   setNum(newNum)
+//   }
+//   return (
+//     <div>
+//       <h1>{num}</h1>
+//       <button onClick={btnClicked}>click</button>
+//     </div>
+//   )
+// }
 
+// export default App
+// 
+
+
+const App = () => {
+  const [num, setNum] = useState({user:'bharat',age:23})
+  
   const btnClicked=()=>{
-  const  newNum = [...num]
-  newNum.push(99)
-  console.log(newNum);
-  setNum(newNum)
+    setNum(prev=>({...prev,age:25})) // prev-> purani value
+
   }
+
+
   return (
     <div>
-      <h1>{num}</h1>
+      <h1>{num.user},{num.age}</h1>
       <button onClick={btnClicked}>click</button>
+
     </div>
   )
 }
