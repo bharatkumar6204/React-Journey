5f33d54df96e504b68a7984fbe37c6a3cdf9492a
bharatkumar6204 <bbharatkusavansi@gmail.com>
2026-05-31 19:11:59 +0530

    09-usestate

--- 09-usestate/src/App.jsx
+++ 09-usestate/src/App.jsx
@@ -1,16 +1,34 @@
 import { useState } from "react"
 
 
+// const App = () => {
+
+//   const [num, setNum] = useState(10)
+//   function changeNum(){
+//     setNum(30) 
+//   }
+//   return (
+//     <div>
+//       <h1>Value of num is {num} </h1>
+//       <button onClick={changeNum} >Click</button>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
 const App = () => {
 
-  const [num, setNum] = useState(10)
-  function changeNum(){
-    setNum(30) 
-  }
+const [user, setUser] = useState('raju')
+function changeUser(){
+  setUser('Bharat')
+}
   return (
     <div>
-      <h1>Value of num is {num} </h1>
-      <button onClick={changeNum} >Click</button>
+      <h1>This user is {user} </h1>
+      <button onClick={changeUser}>Click</button>
     </div>
   )
 }
