622be15cb1a80fd85f67be4cbe89b8456b753b37
bharatkumar6204 <bbharatkusavansi@gmail.com>
2026-06-03 18:24:45 +0530

    localstorage

--- 14-localstorage/src/App.jsx
+++ 14-localstorage/src/App.jsx
@@ -1,14 +1,26 @@
 
 const App = () => {
   // localStorage.clear()
-localStorage.setItem('age','23')
+// localStorage.setItem('age','23')
 
-const age = localStorage.getItem('age')
-console.log(age);
-  
+// const user = localStorage.getItem('user')
+// const age = localStorage.getItem('age')
+// console.log(age,user);
+// localStorage.removeItem('age')
+
+// const user = {
+//   username:'bhrat',
+//   age : 23,
+//   city : 'patna',
+//   course : 'python'
+// }
+// localStorage.setItem('user',JSON.stringify(user))
+
+const user = JSON.parse(localStorage.getItem('user'))
+console.log(user);
+
   return (
-    <div>
-
+    <div>App
     </div>
   )
 }
