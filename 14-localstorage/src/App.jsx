8bdd98fd3ffedbfad8d8156d3518e3a13e07c29f
bharatkumar6204 <bbharatkusavansi@gmail.com>
2026-06-03 18:09:18 +0530

    14-localstorage

---
+++ 14-localstorage/src/App.jsx
@@ -0,0 +1,16 @@
+
+const App = () => {
+  // localStorage.clear()
+localStorage.setItem('age','23')
+
+const age = localStorage.getItem('age')
+console.log(age);
+  
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default App
