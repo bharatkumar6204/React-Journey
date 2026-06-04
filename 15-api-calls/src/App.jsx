b1d5da7ee08a9a2e05f92df80703885b597035fe
bharatkumar6204 <bbharatkusavansi@gmail.com>
2026-06-04 22:50:57 +0530

    15-api-calls

--- 15-api-calls/src/App.jsx
+++ 15-api-calls/src/App.jsx
@@ -15,17 +15,31 @@
 
 //
 import axios from 'axios'
+import { useState } from 'react';
 
 const App = () => {
 
+  const [data, setData] = useState([])
+
+  // const getData = async() => {
+  //  const {data} = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
+  //  console.log(data);
+  // };
+
   const getData = async() => {
-   const {data} = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
-   console.log(data);
-  };
+   const response = await axios.get('https://picsum.photos/v2/list')
+   setData(response.data)
+   
+  }
 
   return (
     <div>
       <button onClick={getData}>Click</button>
+      <div>
+        {data.map(function(e,idx){
+          return <h2>{e.author}{idx} </h2>
+        })}
+      </div>
     </div>
   );
 };
